4ace8de863de5f2f49e059f57e008db763bca015
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2015-06-24 10:58:01 +0200

    benchmark-tests: add cast to big decimal for teradata queries
    simple select count(*) from lineitem for 1TB schema causes Numeric overflow on teradata
    
    Task: SWARM-802
    Testing: manual

--- benchmark-tests/src/main/resources/sql/teradata/linear-scan/selectivity-0.sql
+++ benchmark-tests/src/main/resources/sql/teradata/linear-scan/selectivity-0.sql
@@ -1,3 +1,3 @@
-SELECT COUNT(orderkey), SUM(quantity), AVG(extendedprice)
+SELECT cast(COUNT(orderkey) as decimal(18,2)), cast(SUM(quantity) as decimal(18,2)), cast(AVG(extendedprice) as decimal(18,2))
 FROM "${schema}"."lineitem"
 WHERE quantity < 1
