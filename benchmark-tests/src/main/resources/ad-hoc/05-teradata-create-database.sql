875a708a2f1c3d4502ad11a46d5ceaa807b1e273
Grzegorz Kokosiński <g.kokosinski@gmail.com>
2015-07-02 10:25:20 +0200

    update benchmarks
    * remove drop caches for benchmarks with warm up
    * limit warm up to 2 queries for linear-scan
    * limit runs to 3 queries for linear-scan
    * fix queries (numeric overflow)
    * grant test_presto_user access to select tpch tables in teradata
    
    Pull request: https://github-bdch.td.teradata.com/bdch/benchmark/pull/60

--- benchmark-tests/src/main/resources/ad-hoc/05-teradata-create-database.sql
+++ benchmark-tests/src/main/resources/ad-hoc/05-teradata-create-database.sql
@@ -2,10 +2,13 @@
 
 CREATE DATABASE tpch_10gb AS PERMANENT = 1e11, SPOOL = 1e11;
 GRANT CREATE TABLE ON tpch_10gb TO test_presto_user;
+GRANT SELECT ON tpch_10gb TO test_presto_user;
 
 CREATE DATABASE tpch_100gb AS PERMANENT = 1e12, SPOOL = 1e12;
 GRANT CREATE TABLE ON tpch_100gb TO test_presto_user;
+GRANT SELECT ON tpch_100gb TO test_presto_user;
 
 
 CREATE DATABASE tpch_1tb AS PERMANENT = 1e13, SPOOL = 1e13;
 GRANT CREATE TABLE ON tpch_1tb TO test_presto_user;
+GRANT SELECT ON tpch_1tb TO test_presto_user;
--- benchmark-tests/src/main/resources/sql/querygrid-presto-ansi/linear-scan/selectivity-2.sql
+++ benchmark-tests/src/main/resources/sql/querygrid-presto-ansi/linear-scan/selectivity-2.sql
@@ -1,3 +1,3 @@
-SELECT COUNT(orderkey), SUM(quantity), AVG(extendedprice)
+SELECT CAST(COUNT(orderkey) as bigint), CAST(SUM(quantity) as bigint), CAST(AVG(extendedprice) as bigint)
 FROM "${schema}"."lineitem"@"${database}"
 WHERE quantity < 2
