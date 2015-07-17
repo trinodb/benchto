47eb403faad2fe3e9d821fb4b099b1d5a3cdd6a5
Stanislaw Ogorkis <stanislaw.ogorkis@teradata.com>
2015-07-17 11:19:46 +0200

    Updating benchto endpoints
    Reviewers: ksobczak
    
    Pull request: https://github-bdch.td.teradata.com/bdch/benchmark/pull/99

--- benchmark-tests/src/main/resources/ad-hoc/07-define-backend-service-environments.sh
+++ benchmark-tests/src/main/resources/ad-hoc/07-define-backend-service-environments.sh
@@ -2,18 +2,6 @@
 
 curl -X POST -H 'Content-Type: application/json' -d '{
     "dashboardType": "grafana",
-    "dashboardURL": "http://10.25.17.79:3000/dashboard/db/hdp-cluster",
-    "prestoURL": "http://10.25.17.79:8090/"
-}' http://10.25.17.79:18080/v1/environment/HDP
-
-curl -X POST -H 'Content-Type: application/json' -d '{
-    "dashboardType": "grafana",
-    "dashboardURL": "http://10.25.17.79:3000/dashboard/db/cdh-cluster",
-    "prestoURL": "http://10.25.17.79:8090/"
-}' http://10.25.17.79:18080/v1/environment/CDH
-
-curl -X POST -H 'Content-Type: application/json' -d '{
-    "dashboardType": "grafana",
-    "dashboardURL": "http://10.25.17.79:3000/dashboard/db/td-hdp-cluster",
+    "dashboardURL": "http://benchto.td.teradata.com:3000/dashboard/db/td-hdp-cluster",
     "prestoURL": "http://cloud10hd01-2-2.labs.teradata.com:8888/"
-}' http://10.25.17.79:18080/v1/environment/TD-HDP
+}' http://benchto.td.teradata.com/v1/environment/TD-HDP
