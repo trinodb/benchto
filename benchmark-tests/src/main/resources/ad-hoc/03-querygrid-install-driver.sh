2f5dda98079d2ca0ad7dcc3cc0c04abc4d6104e7
Stanislaw Ogorkis <stanislaw.ogorkis@teradata.com>
2015-07-30 13:32:39 +0200

    Change max spool size for QueryGrid driver install

--- benchmark-tests/src/main/resources/ad-hoc/03-querygrid-install-driver.sh
+++ benchmark-tests/src/main/resources/ad-hoc/03-querygrid-install-driver.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -e;
-hfab $1 querygrid.install:local_path=$2,user_perm_space=1e13,user_spool_space=1e13,user_temp_space=1e13
+hfab $1 querygrid.install:local_path=$2,user_perm_space=1e13,user_spool_space=3e13,user_temp_space=1e13
