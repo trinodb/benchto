fbfb50b123f0c05521807338e5edc954715057e3
Stanislaw Ogorkis <stanislaw.ogorkis@teradata.com>
2015-08-05 15:26:41 +0200

    Fixing idempotency of querygrid driver installation scripts

--- benchmark-tests/src/main/resources/ad-hoc/03-querygrid-install-driver.sh
+++ benchmark-tests/src/main/resources/ad-hoc/03-querygrid-install-driver.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 set -e;
+
+hfab $1 querygrid.drop_server:server_name=presto_server || true
+hfab $1 querygrid.drop_server:server_name=hive || true
+hfab $1 querygrid.drop_server:server_name=tpch || true
+hfab $1 querygrid.uninstall || true
+
 hfab $1 querygrid.install:local_path=$2,user_perm_space=1e13,user_spool_space=3e13,user_temp_space=1e13
--- benchmark-tests/src/main/resources/ad-hoc/04-querygrid-create-server.sh
+++ benchmark-tests/src/main/resources/ad-hoc/04-querygrid-create-server.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -e;
 
-hfab $1 querygrid.drop_server:server_name=presto_server || true
-hfab $1 querygrid.drop_server:server_name=hive || true
-hfab $1 querygrid.drop_server:server_name=tpch || true
-
 hfab $1 querygrid.create_server:server_name=presto_server,presto_master_ip=$2,presto_master_port=$3,hdfs_master_ip=$4
 hfab $1 querygrid.create_server:server_name=hive,presto_master_ip=$2,presto_master_port=$3,hdfs_master_ip=$4,presto_catalog=hive
 hfab $1 querygrid.create_server:server_name=tpch,presto_master_ip=$2,presto_master_port=$3,hdfs_master_ip=$4,presto_catalog=tpch,presto_schema=tiny
