61d0fcd9837423e58575b1ea35ceb1bf11f9aa80
Piotr Nowojski <piotr.nowojski@teradata.com>
2015-09-03 08:22:32 +0200

    Added blackhole foreign servers to scripts
    Reviewers: arosa, sogorkis
    
    Pull request: https://github-bdch.td.teradata.com/bdch/benchmark/pull/114

--- benchmark-tests/src/main/resources/ad-hoc/03-querygrid-install-driver.sh
+++ benchmark-tests/src/main/resources/ad-hoc/03-querygrid-install-driver.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
+set +e
+
+hfab $1 querygrid.drop_server:server_name=hdfs_pushdown
+hfab $1 querygrid.drop_server:server_name=hdfs_hive
+hfab $1 querygrid.drop_server:server_name=hdfs_tpch
+hfab $1 querygrid.drop_server:server_name=hdfs_blackhole
+
+hfab $1 querygrid.drop_server:server_name=greyhound_pushdown
+hfab $1 querygrid.drop_server:server_name=greyhound_hive
+hfab $1 querygrid.drop_server:server_name=greyhound_tpch
+hfab $1 querygrid.drop_server:server_name=greyhound_blackhole
+
+hfab $1 querygrid.uninstall
+
 set -e;
-
-hfab $1 querygrid.drop_server:server_name=hdfs_pushdown || true
-hfab $1 querygrid.drop_server:server_name=hdfs_hive || true
-hfab $1 querygrid.drop_server:server_name=hdfs_tpch || true
-
-hfab $1 querygrid.drop_server:server_name=greyhound_pushdown || true
-hfab $1 querygrid.drop_server:server_name=greyhound_hive || true
-hfab $1 querygrid.drop_server:server_name=greyhound_tpch || true
-
-hfab $1 querygrid.uninstall || true
 
 hfab $1 querygrid.install:local_path=$2,user_perm_space=1e13,user_spool_space=3e13,user_temp_space=1e13
