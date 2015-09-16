90c184d4b107390a1d77c356e93fe88734cfb630
Stanislaw Ogorkis <stanislaw.ogorkis@teradata.com>
2015-09-16 10:36:10 +0200

    Changed size of types benchmarks

--- benchmark-tests/src/main/resources/ad-hoc/10-generate-hive-type-data.sh
+++ benchmark-tests/src/main/resources/ad-hoc/10-generate-hive-type-data.sh
@@ -4,20 +4,16 @@
 # to cluster and run following commands from hdfs user. It can take long time to
 # generate it so it is recommended to use nohup:
 #
-# nohup ./10-generate-hive-type-data.sh 100000000 150 &  # 100M rows, 150 mappers
-# nohup ./10-generate-hive-type-data.sh 1000000000 200 & # 1B rows, 200 mappers
+# nohup ./10-generate-hive-type-data.sh 1000000000 150 &  # 1B rows, 150 mappers
+# nohup ./10-generate-hive-type-data.sh 10000000000 200 & # 10B rows, 200 mappers
 
 GENERATOR_HDFS_FILES_PATH=/benchmarks/benchto/types
 GENERATOR_JAR_FILE=benchto-generator-1.0.0-SNAPSHOT.jar
 DATASET_SIZE=$1
 MAPPERS_COUNT=$2
 
-echo "Removing files under ${GENERATOR_HDFS_FILES_PATH}"
-hdfs dfs -rm -R ${GENERATOR_HDFS_FILES_PATH}
-
-echo "Generating 100M dataset"
 for format in 'text' 'orc'; do
-    for type in 'bigint' 'int' 'double' 'decimal(38,8)' 'boolean' 'varchar(255)' 'date' 'timestamp' 'binary'; do
-        hadoop jar ${GENERATOR_JAR_FILE} ${format} ${type} ${DATASET_SIZE} ${MAPPERS_COUNT}
+    for type in 'bigint' 'int' 'double' 'decimal(38,8)' 'decimal(38,19)' 'decimal(12,4)' 'boolean' 'varchar(255)' 'date' 'timestamp' 'binary'; do
+        hadoop jar ${GENERATOR_JAR_FILE} ${format} ${type} ${DATASET_SIZE} ${MAPPERS_COUNT} &
     done
 done
--- benchmark-tests/src/main/resources/ad-hoc/11-hive_fullfill_alltypes_schema.sql
+++ benchmark-tests/src/main/resources/ad-hoc/11-hive_fullfill_alltypes_schema.sql
@@ -1,27 +1,7 @@
-CREATE DATABASE types_100m_orc;
-CREATE DATABASE types_100m_text;
 CREATE DATABASE types_1b_orc;
 CREATE DATABASE types_1b_text;
-
-CREATE EXTERNAL TABLE types_100m_text.boolean       (value BOOLEAN)        ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-boolean/100000000';
-CREATE EXTERNAL TABLE types_100m_text.int           (value INT)            ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-int/100000000';
-CREATE EXTERNAL TABLE types_100m_text.bigint        (value BIGINT)         ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-bigint/100000000';
-CREATE EXTERNAL TABLE types_100m_text.double        (value DOUBLE)         ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-double/100000000';
-CREATE EXTERNAL TABLE types_100m_text.varchar_255   (value VARCHAR(255))   ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-varchar(255)/100000000';
-CREATE EXTERNAL TABLE types_100m_text.binary        (value BINARY)         ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-binary/100000000';
-CREATE EXTERNAL TABLE types_100m_text.date          (value DATE)           ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-date/100000000';
-CREATE EXTERNAL TABLE types_100m_text.timestamp     (value TIMESTAMP)      ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-timestamp/100000000';
-CREATE EXTERNAL TABLE types_100m_text.decimal_38_8  (value DECIMAL(38,8))  ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-decimal(38,8)/100000000';
-
-CREATE EXTERNAL TABLE types_100m_orc.boolean       (value BOOLEAN)        STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-boolean/100000000';
-CREATE EXTERNAL TABLE types_100m_orc.int           (value INT)            STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-int/100000000';
-CREATE EXTERNAL TABLE types_100m_orc.bigint        (value BIGINT)         STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-bigint/100000000';
-CREATE EXTERNAL TABLE types_100m_orc.double        (value DOUBLE)         STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-double/100000000';
-CREATE EXTERNAL TABLE types_100m_orc.varchar_255   (value VARCHAR(255))   STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-varchar(255)/100000000';
-CREATE EXTERNAL TABLE types_100m_orc.binary        (value BINARY)         STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-binary/100000000';
-CREATE EXTERNAL TABLE types_100m_orc.date          (value DATE)           STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-date/100000000';
-CREATE EXTERNAL TABLE types_100m_orc.timestamp     (value TIMESTAMP)      STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-timestamp/100000000';
-CREATE EXTERNAL TABLE types_100m_orc.decimal_38_8  (value DECIMAL(38,8))  STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-decimal(38,8)/100000000';
+CREATE DATABASE types_10b_orc;
+CREATE DATABASE types_10b_text;
 
 CREATE EXTERNAL TABLE types_1b_text.boolean       (value BOOLEAN)        ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-boolean/1000000000';
 CREATE EXTERNAL TABLE types_1b_text.int           (value INT)            ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-int/1000000000';
@@ -32,6 +12,8 @@ CREATE EXTERNAL TABLE types_1b_text.binary        (value BINARY)         ROW FOR
 CREATE EXTERNAL TABLE types_1b_text.date          (value DATE)           ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-date/1000000000';
 CREATE EXTERNAL TABLE types_1b_text.timestamp     (value TIMESTAMP)      ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-timestamp/1000000000';
 CREATE EXTERNAL TABLE types_1b_text.decimal_38_8  (value DECIMAL(38,8))  ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-decimal(38,8)/1000000000';
+CREATE EXTERNAL TABLE types_1b_text.decimal_38_19 (value DECIMAL(38,19)) ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-decimal(38,19)/1000000000';
+CREATE EXTERNAL TABLE types_1b_text.decimal_12_4  (value DECIMAL(12,4))  ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-decimal(12,4)/1000000000';
 
 CREATE EXTERNAL TABLE types_1b_orc.boolean       (value BOOLEAN)        STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-boolean/1000000000';
 CREATE EXTERNAL TABLE types_1b_orc.int           (value INT)            STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-int/1000000000';
@@ -42,3 +24,29 @@ CREATE EXTERNAL TABLE types_1b_orc.binary        (value BINARY)         STORED A
 CREATE EXTERNAL TABLE types_1b_orc.date          (value DATE)           STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-date/1000000000';
 CREATE EXTERNAL TABLE types_1b_orc.timestamp     (value TIMESTAMP)      STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-timestamp/1000000000';
 CREATE EXTERNAL TABLE types_1b_orc.decimal_38_8  (value DECIMAL(38,8))  STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-decimal(38,8)/1000000000';
+CREATE EXTERNAL TABLE types_1b_orc.decimal_38_19 (value DECIMAL(38,19)) STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-decimal(38,19)/1000000000';
+CREATE EXTERNAL TABLE types_1b_orc.decimal_12_4  (value DECIMAL(12,4))  STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-decimal(12,4)/1000000000';
+
+CREATE EXTERNAL TABLE types_10b_text.boolean       (value BOOLEAN)        ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-boolean/10000000000';
+CREATE EXTERNAL TABLE types_10b_text.int           (value INT)            ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-int/10000000000';
+CREATE EXTERNAL TABLE types_10b_text.bigint        (value BIGINT)         ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-bigint/10000000000';
+CREATE EXTERNAL TABLE types_10b_text.double        (value DOUBLE)         ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-double/10000000000';
+CREATE EXTERNAL TABLE types_10b_text.varchar_255   (value VARCHAR(255))   ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-varchar(255)/10000000000';
+CREATE EXTERNAL TABLE types_10b_text.binary        (value BINARY)         ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-binary/10000000000';
+CREATE EXTERNAL TABLE types_10b_text.date          (value DATE)           ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-date/10000000000';
+CREATE EXTERNAL TABLE types_10b_text.timestamp     (value TIMESTAMP)      ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-timestamp/10000000000';
+CREATE EXTERNAL TABLE types_10b_text.decimal_38_8  (value DECIMAL(38,8))  ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-decimal(38,8)/10000000000';
+CREATE EXTERNAL TABLE types_10b_text.decimal_38_19 (value DECIMAL(38,19)) ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-decimal(38,19)/10000000000';
+CREATE EXTERNAL TABLE types_10b_text.decimal_12_4  (value DECIMAL(12,4))  ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' STORED AS TEXTFILE LOCATION '/benchmarks/benchto/types/text-decimal(12,4)/10000000000';
+
+CREATE EXTERNAL TABLE types_10b_orc.boolean       (value BOOLEAN)        STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-boolean/10000000000';
+CREATE EXTERNAL TABLE types_10b_orc.int           (value INT)            STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-int/10000000000';
+CREATE EXTERNAL TABLE types_10b_orc.bigint        (value BIGINT)         STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-bigint/10000000000';
+CREATE EXTERNAL TABLE types_10b_orc.double        (value DOUBLE)         STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-double/10000000000';
+CREATE EXTERNAL TABLE types_10b_orc.varchar_255   (value VARCHAR(255))   STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-varchar(255)/10000000000';
+CREATE EXTERNAL TABLE types_10b_orc.binary        (value BINARY)         STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-binary/10000000000';
+CREATE EXTERNAL TABLE types_10b_orc.date          (value DATE)           STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-date/10000000000';
+CREATE EXTERNAL TABLE types_10b_orc.timestamp     (value TIMESTAMP)      STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-timestamp/10000000000';
+CREATE EXTERNAL TABLE types_10b_orc.decimal_38_8  (value DECIMAL(38,8))  STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-decimal(38,8)/10000000000';
+CREATE EXTERNAL TABLE types_10b_orc.decimal_38_19 (value DECIMAL(38,19)) STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-decimal(38,19)/10000000000';
+CREATE EXTERNAL TABLE types_10b_orc.decimal_12_4  (value DECIMAL(12,4))  STORED AS ORC LOCATION '/benchmarks/benchto/types/orc-decimal(12,4)/10000000000';
